5899ad3efc43ccd1ad2736141fae32ad41bc1343
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-09-28 15:04:22 -0600

    Storing user settings with AppStorage

--- ContentView.swift
+++ ContentView.swift
@@ -7,47 +7,18 @@
 
 import SwiftUI
 
-struct SecondView: View {
-    @Environment(\.dismiss) var dismiss
-    let parent: String
-    
-    var body: some View {
-        Text("Hello from here too \(parent)")
-        Button("Dismiss") {
-            dismiss()
-        }
-        .buttonStyle(.bordered)
-    }
-}
-
 struct ContentView: View {
-    @State private var numbers = [Int]()
-    @State private var currentNumber = 1
+    @AppStorage("tapCount") private var tapCount = 0
     
     var body: some View {
         NavigationStack {
             VStack {
-                List {
-                    ForEach(numbers, id: \.self) {
-                        Text("Row \($0)")
-                    }
-                    .onDelete(perform: deleteRows)
+                Button("Tap Count \(tapCount)") {
+                    tapCount += 1
                 }
                 
-                Button("Add Number") {
-                    numbers.append(currentNumber)
-                    currentNumber += 1
-                }
-                .toolbar {
-                    EditButton()
-                }
             }
-            .navigationTitle("OnDelete()")
         }
-    }
-    
-    func deleteRows(at offset: IndexSet) {
-        numbers.remove(atOffsets: offset)
     }
 }
 
