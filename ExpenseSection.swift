8c8cd2fef2c4e2c8f16176be8bfe757fe7100558
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-09-30 18:02:42 -0600

    Section to split the types results

---
+++ ExpenseSection.swift
@@ -0,0 +1,28 @@
+//
+//  ExpenseSection.swift
+//  iExpense
+//
+//  Created by Guillermo Ruiz Baires on 30/9/23.
+//
+
+import SwiftUI
+
+struct ExpenseSection: View {
+    let title: String
+    let expenses: [ExpenseItem]
+    let deleteItems: (IndexSet) -> Void
+    
+    var body: some View {
+        Section(title) {
+            ForEach(expenses) { item in
+                ItemRow(name: item.name, type: item.type, amount: item.amount)
+            }
+            .onDelete(perform: deleteItems)
+        }
+    }
+}
+
+#Preview {
+    ExpenseSection(title: "Example", expenses: [],
+                   deleteItems: { _ in })
+}
