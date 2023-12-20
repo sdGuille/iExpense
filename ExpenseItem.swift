1a62a6dd1d450301d565a0350281bac52e244ff5
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-12-20 12:06:06 -0600

    Implementing SwifData
    ExpenseItom is a intentional typo because i want to compare both ways to store data

--- ExpenseItem.swift
+++ ExpenseItem.swift
@@ -6,10 +6,27 @@
 //
 
 import Foundation
+import SwiftData
 
 struct ExpenseItem: Codable, Identifiable, Equatable {
     var id = UUID()
     let name: String
     let type: String
     let amount: Double
+}
+
+
+@Model
+class ExpenseItom {
+    var id = UUID()
+    var name: String
+    var type: String
+    var amount: Double
+    
+    init(id: UUID = UUID(), name: String, type: String, amount: Double) {
+        self.id = id
+        self.name = name
+        self.type = type
+        self.amount = amount
+    }
 }
